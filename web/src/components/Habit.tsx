7f155693fead2448b8767a087b951bc41bfeb4af
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2023-01-25 16:20:39 -0300

    chore: finished frontend configuration

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,5 +1,13 @@
-export function Habit() {
+interface HabitProps {
+  completed: number
+}
+
+export function Habit(props: HabitProps) {
   return (
-    <p>Test</p>
+    <div className='bg-gray-500 w-10 text-green-600 items-center'>
+      <p>{props.completed}</p>
+      <p>azul</p>
+    </div>
+
   )
 };
